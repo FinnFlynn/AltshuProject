f5c364c0b00d58405a47e93db2f94ce2bf4473bf
FinnFlynn <116841077+FinnFlynn@users.noreply.github.com>
2022-11-29 21:09:54 +0300

    Add files via upload

---
+++ HTML/11.11.2022/4.1.js
@@ -0,0 +1,25 @@
+let userAge = prompt('Ваш возраст', [32]);
+let closeAgeForm = false;
+while (closeAgeForm == false) {
+    userAge == 'Отмена'
+        ? closeAgeForm = true
+        : userAge === ''
+            ? (alert('Введите арабскими цифрами без пробелов. Для отмены напечатейте: \'Отмена\''),
+                userAge = prompt('Ваш возраст', [32])
+            )
+            : (userAge *= 1,
+                console.log(userAge),
+                isNaN(userAge)
+                    ? (alert('Введите арабскими цифрами без пробелов. Для отмены напечатейте: \'Отмена\''),
+                        userAge = prompt('Ваш возраст', [32])
+                    )
+                    : (closeAgeForm = true, userAge < 12
+                        ? alert('К сожалению доступ зарещен. Вы слишком молоды..')
+                        : userAge > 99
+                            ? alert('Моё почтение. Добро пожаловать.')
+                            : alert('Добро пожаловать.')
+                    )
+            );
+}
+closeAgeForm = false
+console.log('OK')
